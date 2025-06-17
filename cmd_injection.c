85b3d05c2557c6bd5379b3d60234990feff459d2
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-17 18:40:16 +0800

    fall back to original cmd_injection.c

--- cmd_injection.c
+++ cmd_injection.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
-    #include <string.h>
+#include <unistd.h>
 
-    int main(int argc, char **argv) {
-        char cat[] = "cat ";
-        char name[17]; // Increase size by 1 to accommodate null terminator
-        char *command;
-        size_t commandLength;
+int main(int argc, char **argv) {
 
-        printf("What's your name?\n");
-        fgets(name, sizeof(name), stdin);
-        name[strcspn(name, "\n")] = '\0'; // Remove newline character if present
+char cat[] = "cat ";
+char *command;
+size_t commandLength;
 
-        commandLength = strlen(cat) + strlen(name) + 1;
-        command = (char *) malloc(commandLength);
-        strncpy(command, cat, commandLength);
-        strncat(command, name, (commandLength - strlen(cat)));
+commandLength = strlen(cat) + strlen(argv[1]) + 1;
+command = (char *) malloc(commandLength);
+strncpy(command, cat, commandLength);
+strncat(command, argv[1], (commandLength - strlen(cat)) );
 
-        system(command);
-        free(command); // Free allocated memory
-
-        return 0;
-    }
+system(command);
+return (0);
+}
