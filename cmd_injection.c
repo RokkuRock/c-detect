be0b1134e618e1b7bd80234d999e03675eb642f5
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-17 18:32:37 +0800

    Nxcode-CQ-7B-orpo (SCOT) output fix

--- cmd_injection.c
+++ cmd_injection.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-#include <unistd.h>
+    #include <string.h>
 
-int main(int argc, char **argv) {
+    int main(int argc, char **argv) {
+        char cat[] = "cat ";
+        char name[17]; // Increase size by 1 to accommodate null terminator
+        char *command;
+        size_t commandLength;
 
-char cat[] = "cat ";
-char *command;
-size_t commandLength;
+        printf("What's your name?\n");
+        fgets(name, sizeof(name), stdin);
+        name[strcspn(name, "\n")] = '\0'; // Remove newline character if present
 
-commandLength = strlen(cat) + strlen(argv[1]) + 1;
-command = (char *) malloc(commandLength);
-strncpy(command, cat, commandLength);
-strncat(command, argv[1], (commandLength - strlen(cat)) );
+        commandLength = strlen(cat) + strlen(name) + 1;
+        command = (char *) malloc(commandLength);
+        strncpy(command, cat, commandLength);
+        strncat(command, name, (commandLength - strlen(cat)));
 
-system(command);
-return (0);
-}
+        system(command);
+        free(command); // Free allocated memory
+
+        return 0;
+    }
