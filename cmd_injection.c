92137d8e19bd31884d84149d380fd8d9392f060d
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-17 18:05:13 +0800

    fall back to original  CWE-78 cmd_injection.c

--- cmd_injection.c
+++ cmd_injection.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    if (argc <= 1) {
-        printf("Usage: %s filename\n", argv[0]);
-        return EXIT_FAILURE;
-    }
 
-    size_t len = strlen(argv[1]);
-    char *cmd = calloc(len+5, sizeof(*cmd));
-    sprintf(cmd, "cat %s", argv[1]);
+char cat[] = "cat ";
+char *command;
+size_t commandLength;
 
-    system(cmd);
-    free(cmd);
-    return EXIT_SUCCESS;
+commandLength = strlen(cat) + strlen(argv[1]) + 1;
+command = (char *) malloc(commandLength);
+strncpy(command, cat, commandLength);
+strncat(command, argv[1], (commandLength - strlen(cat)) );
+
+system(command);
+return (0);
 }
