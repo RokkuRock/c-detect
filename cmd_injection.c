9cc5829a2d4679f477c7834b1c34b13e1f4deb01
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-17 18:44:03 +0800

    CodeQwen1.5-7B-Chat (OSDP) output fix

--- cmd_injection.c
+++ cmd_injection.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
+void safeCat(const char* filename) {
+    pid_t pid = fork();
+    if (pid == 0) { // Child process
+        execlp("cat", "cat", filename, NULL);
+        perror("Failed to execute 'cat'");
+        exit(-1);
+    } else if (pid > 0) { // Parent process
+        int status;
+        wait(&status);
+        if (WIFEXITED(status) && WEXITSTATUS(status)!= 0) {
+            fprintf(stderr, "'cat %s' failed with exit code %d\n", filename, WEXITSTATUS(status));
+        }
+    } else { // Fork failed
+        perror("Fork failed");
+    }
+}
+
 int main(int argc, char **argv) {
-
-char cat[] = "cat ";
-char *command;
-size_t commandLength;
-
-commandLength = strlen(cat) + strlen(argv[1]) + 1;
-command = (char *) malloc(commandLength);
-strncpy(command, cat, commandLength);
-strncat(command, argv[1], (commandLength - strlen(cat)) );
-
-system(command);
-return (0);
+    if (argc <= 1) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return (-1);
+    }
+    safeCat(argv[1]);
+    return (0);
 }
