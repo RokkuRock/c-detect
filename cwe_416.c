66dc95408dbe5598760119f4b6c9a63908870810
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-20 15:04:32 +0800

    Update cwe_416.c

--- cwe_416.c
+++ cwe_416.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #define BUFSIZER1 512
 #define BUFSIZER2 ((BUFSIZER1/2) - 8)
-
 int main(int argc, char **argv) {
     char *buf1R1;
     char *buf2R1;
@@ -11,12 +10,17 @@ int main(int argc, char **argv) {
 
     buf1R1 = (char *) malloc(BUFSIZER1);
     buf2R1 = (char *) malloc(BUFSIZER1);
+    free(buf2R1);
     buf2R2 = (char *) malloc(BUFSIZER2);
     buf3R2 = (char *) malloc(BUFSIZER2);
 
-    strncpy(buf2R1, argv[1], BUFSIZER1 - 1);
+    // Fix: 使用新的指针进行strncpy操作
+    char *new_buf = (char *) malloc(BUFSIZER1);
+    strncpy(new_buf, argv[1], BUFSIZER1 - 1);
     free(buf1R1);
-    free(buf2R1);
     free(buf2R2);
     free(buf3R2);
+    free(new_buf);
+
+    return 0;
 }
